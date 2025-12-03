4a7a9c4327c46cb639faa4a745cb249c8524f8b4
MOhammed <muhammedaman1314@gmail.com>
2025-12-03 20:55:43 +0530

    Remove conflicting api/index.js file

--- api/index.js
+++
@@ -1,7 +0,0 @@
-// Vercel Serverless Function Entry Point
-import('../dist/index.js').then(module => {
-  // Export the Express app as a serverless function
-  module.default;
-}).catch(err => {
-  console.error('Failed to load server:', err);
-});
